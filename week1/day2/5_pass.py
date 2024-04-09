5479bb7bb547e850a980ce63f095f07b92103086
harshjaiswal01 <harshjaiswal@gmail.com>
2024-04-09 17:46:23 -0600

    Day2

---
+++ week1/day2/5_pass.py
@@ -0,0 +1,10 @@
+#Pass statement is a placeholder key word or temporary standing for codeblocks
+
+feeling = "sick"
+
+if feeling == "sick":
+    pass
+elif feeling == "decent":
+    # pass
+else:
+    print("Chill")
